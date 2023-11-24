f9ac27b4869256a28992a2ea271ac36fc3ee4797
Anyikabz <114746509+Anyikabs@users.noreply.github.com>
2023-11-24 04:36:28 +0100

    Add files via upload

--- 0x06-unittests_in_js/8-api/api.js
+++ 0x06-unittests_in_js/8-api/api.js
@@ -1,1 +1,15 @@
-f
+const express = require('express');
+
+const app = express();
+
+const port = 7865;
+
+app.get('/', (req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.send('Welcome to the payment system');
+});
+
+app.listen(port, () => {
+  console.log('API available on localhost port 7865');
+});
